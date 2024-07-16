c38a10366d467edb801a1971346bf2d97bb73986
Jhin-dev <diegormve@gmail.com>
2024-07-16 17:28:13 -0300

    Update app.py

--- my-app/app.py
+++ my-app/app.py
@@ -1,7 +1,5 @@
 from flask import Flask
 from decouple import config
 
-def create_app():
-  app = Flask(__name__)
-  application = app
-  app.secret_key = config('SECRET_KEY')
+app = Flask(__name__)
+app.secret_key = config('SECRET_KEY')
