d9f6e9ddb53bd43c2d267e56da2c480ea043ebfa
Jhin-dev <diegormve@gmail.com>
2024-07-16 17:26:56 -0300

    Update run.py

--- my-app/run.py
+++ my-app/run.py
@@ -1,5 +1,4 @@
 # Declarando nombre de la aplicación e inicializando, crear la aplicación Flask
-from app import app
 from decouple import config
 
 # Importando todos mis Routers (Rutas)
@@ -7,8 +6,11 @@ from routers.router_login import *
 from routers.router_home import *
 from routers.router_page_not_found import *
 
-
+def create_app():
+    init = app
+    return init
 
 # Ejecutando el objeto Flask
 if __name__ == '__main__':
-    app.run(debug=True, port=config('PORT'))
+    init = create_app()
+    init.run(debug=True, port=config('PORT'))
